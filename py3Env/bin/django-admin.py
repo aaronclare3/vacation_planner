984a70d40cefcdf07ae73dae342d40f4a85f7306
Aaron Clare <AaronClare@Aarons-MacBook-Air-2.local>
2019-08-18 23:11:13 -0700

    fixed .gitignore

--- py3Env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/AaronClare/Desktop/coding dojo/Python/django/django_full_stack/belt_exam/py3Env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
